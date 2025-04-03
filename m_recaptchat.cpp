fd38b7011d698d34c745be88fefb66e66e5681d1
reverse <129334336+revrsefr@users.noreply.github.com>
2025-04-03 10:05:20 +0200

    Update m_recaptchat.cpp

--- m_recaptchat.cpp
+++ m_recaptchat.cpp
@@ -6,7 +6,7 @@
  *
  * This file is part of InspIRCd. InspIRCd is free software; you can
  * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; version 2.
+ * License as published by the Free Software Foundation; version 2. MERMEWÑNDLHBDJSKAO HHDSALKYHDASLKYHVA
  */
 
 /// $ModAuthor: reverse Chevronnet <mike.chevronnet@gmail.com>
