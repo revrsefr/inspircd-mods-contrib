8e807cb78cf627fcf6fa5030ba63a06a518b0ac2
reverse <mike.chevronnet@gmail.com>
2025-12-18 16:20:49 +0000

    WHOIS: show port and connect class

--- m_whoisport.cpp
+++ m_whoisport.cpp
@@ -17,21 +17,42 @@
  */
 
 /// $ModAuthor: reverse <mike.chevronnet@gmail.com>
-/// $ModDesc: Adds the port of the user to the WHOIS response for operators only.
+/// $ModDesc: Adds the port and connect class of the user to WHOIS for operators only.
 /// $ModDepends: core 4
 
 #include "inspircd.h"
 #include "modules/whois.h"
+#include "extension.h"
 
 class ModuleWhoisPort final
 	: public Module
 	, public Whois::EventListener
 {
 public:
+	StringExtItem portext;
+	StringExtItem classext;
 	ModuleWhoisPort()
-		: Module(VF_OPTCOMMON, "Adds the port number of the user to the WHOIS response for operators only.")
+		: Module(VF_OPTCOMMON, "Adds the port and connect class of the user to WHOIS for operators only.")
 		, Whois::EventListener(this)
+		, portext(this, "whoisport", ExtensionType::USER, true) // synced
+		, classext(this, "whoisclass", ExtensionType::USER, true) // synced
 	{
+	}
+
+	void OnUserConnect(LocalUser* user) override
+	{
+		portext.Set(user, ConvToStr(user->server_sa.port()), true);
+		const auto& klass = user->GetClass();
+		if (klass)
+			classext.Set(user, klass->GetName(), true);
+	}
+
+	void OnChangeConnectClass(LocalUser* user, const std::shared_ptr<ConnectClass>& klass, bool) override
+	{
+		if (klass)
+			classext.Set(user, klass->GetName(), true);
+		else
+			classext.Unset(user);
 	}
 
 	void OnWhois(Whois::Context& whois) override
@@ -39,22 +60,22 @@ public:
 		User* source = whois.GetSource();
 		User* target = whois.GetTarget();
 
-		// Only show port information if the requesting user (source) is an IRC operator with privs.
-		if (!user->HasPrivPermission("users/auspex"))
-            {
-                user->WriteNotice("You do not have permission to use this command.");
-                return CmdResult::FAILURE;
-            }
-		// Check if the target user is local or remote.
-		LocalUser* luser = IS_LOCAL(target);
-		if (!luser)
+		// Only show information if the requesting user (source) is an IRC operator with users/auspex privs.
+		if (!source->IsOper() || !source->HasPrivPermission("users/auspex"))
 			return;
 
-		// Get the port the user is connected on.
-		int port = luser->server_sa.port();
+		// Get data from synced extensions (works for local and remote users).
+		const std::string* portstr = portext.Get(target);
+		const std::string* classstr = classext.Get(target);
 
-		// Send the port information in the WHOIS response, but only for operators.
-		whois.SendLine(RPL_WHOISSPECIAL, "*", "is using port " + ConvToStr(port));
+		if (!portstr || portstr->empty())
+			return;
+
+		std::string line = "is using port " + *portstr;
+		if (classstr && !classstr->empty())
+			line += " and connect class: " + *classstr;
+
+		whois.SendLine(RPL_WHOISSPECIAL, line);
 	}
 };
 
